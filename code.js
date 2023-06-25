f25ca401a3a6331de1842040aa4d43cf807484c0
Thentrixxx <133041011+Thentrixxx@users.noreply.github.com>
2023-06-25 13:26:23 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,23 +1,33 @@
+//track the current interval
 var intervalId = 0;
 
+//function fires when the user clicks the start button
+//image will move randomly around the screen every second
 function start(){
-    var image = document.getElementById(chc7tcnwzoza1.jpg);
-
+    //pointer to the img on the index page
+    var image = document.getElementById("memeImage");
+    var changeX = 5;
+    var changeY = 5;
+    //this code will move the image around by changing the top and left coordinates
+    //this code will run every 1 second
     intervalId = setInterval(function(){
-        var changeX = getRandomNumber();
-        var changeY = getRandomNumber();
+        //var changeX = getRandomNumber();
+        //var changeY = getRandomNumber();
 
         image.style.left = changeX + "px";
         image.style.top = changeY + "px";
 
         console.log("x=" + changeX);
         console.log("y=" + changeY);
-    }, 1000)
+
+        changeX += 5;
+        changeY += 5;
+    }, 1000);
 }
 
 //get random number
 function getRandomNumber(){
-    return Math.floor(Math.random() * 800)
+    return Math.floor(Math.random() * 800);
 }
 
 //function fires when the user clicks the stop button
